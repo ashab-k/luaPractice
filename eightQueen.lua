a21653d4217a98a55e5ff637a9263f20585b8161
ashab-k <ashabkhan2000@gmail.com>
2025-12-25 02:02:33 +0530

    feat: 8queens program

---
+++ eightQueen.lua
@@ -0,0 +1,40 @@
+N = 8
+
+function isplaceok(a, n, c)
+    for i = 1 , n - 1 do 
+        if (a[i] == c) or (a[i] - i == c - n) or (a[i] + i == c + n) then 
+            return false
+        end 
+    end
+    return true
+end
+
+-- print a board
+
+function printsolution(a)
+    for i = 1 , N do
+        for j = 1, N do
+            io.write(a[i] == j and "X" or "-", " ")
+        end
+        io.write('\n')
+    end 
+    io.write('\n')
+end
+
+-- add to board 'a' all queens from 'n' to 'N'
+
+function addqueen(a,n)
+    if n > N then 
+        printsolution(a)
+    else 
+        for c = 1 , N do
+            if isplaceok(a, n, c) then 
+                a[n] = c
+                addqueen(a , n+1)
+            end
+        end
+    end
+end
+
+
+addqueen({} , 1)
