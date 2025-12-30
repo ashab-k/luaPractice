d14d38aeb4f62f6b2b213d9c5f7d21abe7e612c1
ashab-k <ashabkhan2000@gmail.com>
2025-12-30 23:54:51 +0530

    wordcount program

--- poly.lua
+++ poly.lua
@@ -1,10 +1,14 @@
-poly = { 1 , 2 , 3 }
-
-res = 0
-x = 2
-
-for i = 0 , #poly - 1 do
-    res = res + poly[i+1] * (x ^ i)
+function newpoly(poly)
+    return function (x)
+        res = 0
+        for i = 0 , #poly - 1 do
+            res = res + poly[i + 1]* x ^ i
+        end
+        return res
+    end
 end
 
-print(res)
+p = newpoly({3 , 0 , 1})
+print(p(0))
+print(p(5))
+print(p(10))
