e29e3c790478ce57b15b615f667b8e793376aca4
ashab-k <ashabkhan2000@gmail.com>
2026-01-01 01:30:49 +0530

    graphs

--- doublequeue.lua
+++ doublequeue.lua
@@ -1,5 +1,5 @@
 function listNew()
-    return {first = 0 , last = -1}
+    return {first = 0 , last = 0}
 end
 
 function pushFirst( list , value)
@@ -15,20 +15,38 @@ function pushLast (list , value)
 end
 
 function popFirst(list)
+ if list.first > list.last  then error("list is empty") end
  local first = list.first
- if first > list.last then error("list is empty") end
  local value = list[first]
  list[first] = nil
  list.first = first + 1
+ while list[list.first] == nil and list.first <= list.last do 
+    list.first = list.first + 1
+ end
+
+ if list.first > list.last then 
+    list.first = 0 
+    list.last = 0
+ end
  return value
 end
 
 function popLast (list)
+ if list.first > list.last then error("list is empty") end
  local last = list.last
- if list.first > last then error("list is empty") end
+ 
  local value = list[last]
  list[last] = nil
  list.last = last - 1
+
+ while list[list.last] == nil and list.last >= list.first do 
+    list.last = list.last - 1
+ end
+
+ if list.first > list.last then 
+    list.first = 0
+    list.last = 0
+ end
  return value
 end
 
@@ -39,3 +57,7 @@ pushLast(list , 7)
 
 print(popLast(list))
 print(popFirst(list))
+print(popFirst(list))
+print(list.first , list.last)
+
+
